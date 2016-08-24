82a5224383f853f26d44234ee260056691ccebf8
gauravkhare <GA358085@wipro.com>
2016-08-24 21:09:16 +0530

    Added streaming

--- spark/src/main/scala/org/test/spark/WordCount.scala
+++ spark/src/main/scala/org/test/spark/WordCount.scala
@@ -2,6 +2,7 @@ package org.test.spark
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.log4j._
 /**
  *  Refer this link to download winutils.exe http://stackoverflow.com/questions/19620642/failed-to-locate-the-winutils-binary-in-the-hadoop-binary-path
  * Eclipse->Debug/Run Configurations -> Environment (tab) -> and added
@@ -16,9 +17,8 @@ object WordCount {
   
   def main(args: Array[String])={
     
-    val conf = new SparkConf().set("hadoop.home.dir", "D:\\spark-1.5.2-bin-hadoop2.6\\winutils").setAppName("WordCount").setMaster("local");
+    val conf = new SparkConf().setAppName("WordCount").setMaster("local");
     val sc = new SparkContext(conf);
-    
     val test = sc.textFile("food.txt");
     
     test.flatMap { line => line.split(" ") }.map { word => (word,1) }.reduceByKey(_ + _).saveAsTextFile("food.count.txt")
